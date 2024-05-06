29805399ca146f8d73e6022e41577518c4a62470
oAllanWeb <allanweb019@gmail.com>
2024-05-06 19:31:41 -0300

    api

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const User = require('./models/User');
+const port = 3000;
+const db = require('./db');
+app.use(express.json());
+
+db.sync();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  res.send(user);
+});
+
+app.listen(port, () => {
+  console.log(`app on http://localhost:${port}`);
+});
